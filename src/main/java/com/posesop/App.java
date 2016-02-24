4c93348980351a717e3a7c46042dfc446f8c01e7
Victor Pose <vm.pose@mnemo.com>
2016-02-24 14:49:56 +0100

    Primera subida

---
+++ src/main/java/com/posesop/App.java
@@ -0,0 +1,16 @@
+package com.posesop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+ 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class App {
+ 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
